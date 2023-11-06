bca3f5a4d065a5877d43620c0e24ac013459f708
dayicruz <dayicruz1401@gmail.com>
2023-11-06 22:47:08 +0100

    setup + footer desktop

---
+++ src/components/Home/HomeImage.jsx
@@ -0,0 +1,17 @@
+
+import React from 'react';
+import './HomeImage.scss';
+
+
+
+const HomeImage = () => {
+  return (
+    <div className='imgHome' >
+        <div className='imgHome__carousel'>
+           
+        </div>
+    </div>
+  )
+}
+
+export default HomeImage
---
+++ src/pages/Home.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import HomeImage from '../components/Home/HomeImage';
+import Footer from '../components/UI/footer/Footer';
+const Home = () => {
+  return (
+    <>
+    
+    <HomeImage />
+    <Footer/>
+    
+    </>
+   
+  )
+}
+
+export default Home;
