c39daf4553ddb9400a165f9892b312e55d8f9596
dayicruz <dayicruz1401@gmail.com>
2023-12-19 18:18:51 +0100

    lista pagina search con responsive

--- src/components/Search/SearchCategories.jsx
+++ src/components/Search/SearchCategories.jsx
@@ -2,7 +2,7 @@ import './searchCategories.scss'
 
 export const SearchCategories = () => {
   return (
-    <div className="container">
+    <div className="containerSearchCategories">
       <button class="bn632-hover bn23">Popular</button>
       <button class="bn632-hover bn23">Especies</button>
       <button class="bn632-hover bn23">Region</button>
